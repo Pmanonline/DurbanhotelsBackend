181c19b98a3e813d71310e928f79c9693ad8bf76
Pmanonline <petersonzoconis@gmail.com>
2026-02-13 09:19:07 +0100

    pushing changes

--- src/config/allowedOrigins.config.ts
+++ src/config/allowedOrigins.config.ts
@@ -2,6 +2,7 @@ const allowedOrigins = [
   process.env.FRONTEND_URL,
   "https://qr-genius-frontend.vercel.app",
   "http://localhost:3000",
+  "http://10.94.172.99:3000",
 ].filter(Boolean);
 
 export default allowedOrigins;
