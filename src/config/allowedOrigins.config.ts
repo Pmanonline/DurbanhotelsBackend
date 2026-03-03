1563ad4e877e0c58899dbd0143423cdf24d72ef0
Pmanonline <petersonzoconis@gmail.com>
2026-03-03 08:18:15 +0100

    updated cors error

--- src/config/allowedOrigins.config.ts
+++ src/config/allowedOrigins.config.ts
@@ -2,7 +2,7 @@ const allowedOrigins = [
   process.env.FRONTEND_URL,
   "http://localhost:5173",
   "http://localhost:3000",
-  " http://10.89.203.99:5173",
+  "http://10.89.203.99:5173",
   "https://durbanhotels.vercel.app",
 ].filter(Boolean);
 
